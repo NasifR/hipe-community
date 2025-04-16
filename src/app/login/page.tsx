2e65e2fc4305d2db3adf143ddbb97d8b6c5be297
unknown <nasifjuhayer@gmail.com>
2025-04-16 17:16:24 -0400

    login

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -30,6 +30,15 @@ export default function Login() {
         }
     };
 
+    return (
+        <div className="min-h-screen flex items-center justify-center bg-gray-100 px-4 ">
+            <div className='bg-white p-8 rounded-2xl shadow-lg w-full max-w-md'>
+                <h1 className='text-2xl font-semibold mb-6 text-gray-900 text-center'>Log in to your account</h1>
+
+                
+            </div>
+        </div>
+    )
     
     }
 
