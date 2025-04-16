68622742030abbee83561c634b0d64f7d3de2c53
unknown <nasifjuhayer@gmail.com>
2025-04-16 17:26:35 -0400

    login

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -35,10 +35,33 @@ export default function Login() {
             <div className='bg-white p-8 rounded-2xl shadow-lg w-full max-w-md'>
                 <h1 className='text-2xl font-semibold mb-6 text-gray-900 text-center'>Log in to your account</h1>
 
+                <div className="space-y-4">
+          <input
+            name="email"
+            placeholder="Email"
+            onChange={handleChange}
+            type="email"
+            className="w-full px-4 py-2 border border-gray-500 rounded-lg text-gray-900 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
+          />
+          <input
+            name="password"
+            placeholder="Password"
+            onChange={handleChange}
+            type="password"
+            className="w-full px-4 py-2 border border-gray-500 rounded-lg text-gray-900 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
+          />
+        </div>
+
+        <button
+          onClick={handleLogin}
+          className="mt-5 w-full bg-blue-600 text-white py-2 rounded-lg hover:bg-blue-800 transition-all duration-300"
+        >
+          Log in
+        </button>
                 
             </div>
         </div>
-    )
+    );
     
     }
 
