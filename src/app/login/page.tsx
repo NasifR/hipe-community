cefa6e92fad2924488dbb5a25f4a0c317aab3247
unknown <nasifjuhayer@gmail.com>
2025-04-17 12:15:41 -0400

    login

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,41 +1,51 @@
-import { useState } from 'react';
-import { useRouter } from 'next/navigation'
-import { signInWithEmailAndPassword } from 'firebase/auth';
+"use client";
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../../../lib/firebaseConfig";
+import Link from "next/link";
+import logo from "../../../public/images/hipe.png"
+import Image from "next/image";
 
 export default function Login() {
-    const router = useRouter();
+  const router = useRouter();
 
-    type FormData = {
-        email: string;
-        password: string;
-    };
+  type FormData = {
+    email: string;
+    password: string;
+  };
 
-    const [formData, setFormData] = useState<FormData>({
-        email: "",
-        password: "",
-    });
+  const [formData, setFormData] = useState<FormData>({
+    email: "",
+    password: "",
+  });
 
-    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        const { name, value } = e.target;
-        setFormData((prev) => ({ ...prev, [name]: value }));
-    };
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setFormData((prev) => ({ ...prev, [name]: value }));
+  };
 
-    const handleLogin = async () => {
-        try {
-            await signInWithEmailAndPassword(auth, formData.email, formData.password);
-            router.push("/");
-        } catch (error) {
-            console.log(error);
-            alert("Login failed. Error: " + error);
-        }
-    };
+  const handleLogin = async () => {
+    try {
+      await signInWithEmailAndPassword(auth, formData.email, formData.password);
+      router.push("/");
+    } catch (error) {
+      console.log(error);
+      alert("Login failed. Error: " + error);
+    }
+  };
 
-    return (
-        <div className="min-h-screen flex items-center justify-center bg-gray-100 px-4 ">
-            <div className='bg-white p-8 rounded-2xl shadow-lg w-full max-w-md'>
-                <h1 className='text-2xl font-semibold mb-6 text-gray-900 text-center'>Log in to your account</h1>
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100 px-4 ">
+      <div className="bg-white p-8 rounded-2xl shadow-lg w-full max-w-md">
+        <div className="flex justify-center items-center">
+          <Image src={logo} alt="Logo" width={150} height={150} />
+        </div>
+        <h1 className="text-2xl font-semibold mb-6 text-gray-900 text-center">
+          Log in to your account
+        </h1>
 
-                <div className="space-y-4">
+        <div className="space-y-4">
           <input
             name="email"
             placeholder="Email"
@@ -58,11 +68,14 @@ export default function Login() {
         >
           Log in
         </button>
-                
-            </div>
-        </div>
-    );
-    
-    }
-
-    
+        <Link href="/signup">
+          <button
+          className="mt-5 w-full bg-white text-blue-600 py-2 rounded-lg hover:bg-blue-800 hover:text-white border border-blue-600transition-all duration-300"
+          >
+            Create an account
+          </button>
+          </Link>
+      </div>
+    </div>
+  );
+}
